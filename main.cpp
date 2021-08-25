fbef5930c5dce6deb4708507652754e349f3e9b0
Aditya Chaturvedi <68045994+vooop-binary@users.noreply.github.com>
2021-08-25 20:56:51 +0530

    ex 2.1 completed

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <spdlog/spdlog.h>
@@ -53,7 +52,7 @@ int main(int argc, const char** argv) {
     }
 
     glfwMakeContextCurrent(window);
-    glfwSetFramebufferSizeCallback(window, framebufferSizeCallback); // resize
+    glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);  // resize
 
     // initialize all opengl function pointers
     if (glewInit() != GLEW_OK) {
@@ -62,8 +61,6 @@ int main(int argc, const char** argv) {
     }
 
     spdlog::info(glGetString(GL_VERSION));
-
-    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
     // create opengl viewport
     glViewport(0, 0, 800, 600);
@@ -93,56 +90,50 @@ int main(int argc, const char** argv) {
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
-    // rectangle vertices
+    // triangle vertices
     float vertices[] = {
-        // x    y     z
-         0.5f,  0.5f, 0.0f, // top right
-         0.5f, -0.5f, 0.0f, // bottom right
-        -0.5f, -0.5f, 0.0f, // bottom left
-        -0.5f,  0.5f, 0.0f, // top left
-    };
+        // first triangle
+        -0.9f, -0.5f, 0.0f,  // left 
+        -0.0f, -0.5f, 0.0f,  // right
+        -0.45f, 0.5f, 0.0f,  // top 
 
-    unsigned int indices[] = {
-        0, 1, 3, // first triangle
-        1, 2, 3, // second triangle
-    };
+        // second triangle
+        -0.5f, -0.5f, 0.0f,  // left 
+         0.5f, -0.5f, 0.0f,  // right
+         0.0f,  0.5f, 0.0f}; // top
 
-    unsigned int VBO; // vertex buffer object 
-    unsigned int VAO; // vertex array object
+    unsigned int VBO;  // vertex buffer object
+    unsigned int VAO;  // vertex array object
 
     // create vertex array and buffer objects
-    glGenVertexArrays(1, &VAO); // vertex array first
+    glGenVertexArrays(1, &VAO);  // vertex array first
     glGenBuffers(1, &VBO);
 
     // bind objects
     glBindVertexArray(VAO);
+
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
-    unsigned int EBO; // element buffer object
-    glGenBuffers(1, &EBO);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-   
-   // add data to buffer object
+    // add data to buffer object
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
     // tell opengl how to interpret the vertex data
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0); 
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+
     glEnableVertexAttribArray(GL_FALSE);
 
     // main loop
     while (!glfwWindowShouldClose(window)) {
-        processInput(window); // keyboard input
+        processInput(window);  // keyboard input
 
         // add color
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // draw rectangle
+        // draw triangle
         glUseProgram(shaderProgram);
-        glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        // glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
 
         // io
         glfwSwapBuffers(window);
@@ -152,7 +143,6 @@ int main(int argc, const char** argv) {
     // cleanup
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
-    glDeleteBuffers(1, &EBO);
     glDeleteProgram(shaderProgram);
 
     glfwTerminate();
